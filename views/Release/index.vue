64fa63c46c5f23ca0e78da6b296730a818d6c8fc
Dimly-X <1219658407@qq.con>
2022-05-25 22:40:57 +0800

    release-update2

--- views/Release/index.vue
+++ views/Release/index.vue
@@ -2,12 +2,12 @@
     <div>
         <div class="sticky">
             <br>请选择要发布成绩的课程：
-            <el-select v-model="value" placeholder="请选择">
+            <el-select v-model="class_selected" placeholder="请选择" @change="changeData()">
                 <el-option
                 v-for="item in releaseList"
-                :key="item"
-                :label="item"
-                :value="item">
+                :key="item.curriculum_id"
+                :label="item.curriculum_name"
+                :value="item.curriculum_id">
                 </el-option>
             </el-select>
         </div>
@@ -29,13 +29,24 @@
                 </el-table-column>
 
                 <el-table-column
-                label="操作"
+                label="打分"
                 align="center"
                 header-align="center"
                 >
-                <template>
-                    <div class="operation">
-                        <el-link type="primary" class="l-content">打分</el-link>
+                <template slot-scope="scope">
+                    <div class="input-suffix">
+                        <p class="p1">平时成绩：</p>
+                        <el-input
+                            class="input1"
+                            placeholder="请输入"
+                            v-model="input1">
+                        </el-input>
+                        <p class="p2">期末成绩：</p>
+                        <el-input
+                            class="input2"
+                            placeholder="请输入"
+                            v-model="input2">
+                        </el-input>
                     </div>
                 </template>
                 </el-table-column>
@@ -49,6 +60,7 @@
 
 import axios from "axios"
 import { getRelease } from '../../api/data'
+import { getStudentList } from '../../api/data'
 
 export default{
 
@@ -58,23 +70,32 @@ export default{
             releaseList:[],
             tableLabel: {
                 user_id: '学号',
-                user_name: '姓名'
+                user_name: '姓名',
             },
-            value: '',
-            tableData:[
-                {
-                    user_id: '10195101564',
-                    user_name: 'xxx'
-                }
-            ],
+            class_selected: '',
+            tableData:[],
+            input1: '',
+            input2: '',
         }
     },
     methods:{
         getClass(){
             getRelease().then(({ data:res }) => {
-                this.releaseList = res
+                this.releaseList = res;
                 console.log(res,'res')
             })
+        },
+        changeData(){
+            console.log('okkk','res')
+            getStudentList({
+                curriculum_id:this.class_selected
+            }).then(({ data:res }) => {
+                this.tableData = res;
+                console.log(res,'res')
+            })
+        },
+        changeScore(){
+            
         }
     },
     created() {
@@ -88,5 +109,17 @@ export default{
 .table{
     margin: 20px auto 0px auto;
 }
-
+.p1{
+    display: inline;
+}
+.input1{
+    width: 80px;
+}
+.p2{
+    display: inline;
+    margin: 0 0 0 20px;
+}
+.input2{
+    width: 80px;
+}
 </style>
